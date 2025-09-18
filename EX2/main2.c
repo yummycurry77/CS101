563bb96c75a43162b17250b99b4af521e13b0996
yummycurry77 <k0935225710@gmail.com>
2025-09-18 15:36:07 +0800

    Create main2.c

---
+++ EX2/main2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    char a=5;
+    printf("\t%d\n", a^1);
+    printf("\t%d\n", ~a);
+    printf("\t%d\n", a>>1);
+    printf("\t%d\n", a<<1);
+    return 0;
+}
