41f395056c92ca15e6e24363d970a70a0268347b
yummycurry77 <k0935225710@gmail.com>
2025-10-16 15:49:04 +0800

    Create main7.c

---
+++ ex5/main7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int factorial(int n){
+    if(n==1) return 1;
+    return n * factorial(n-1);
+}
+
+
+int main()
+{
+    int n = 5;
+    int m = factorial(n);
+    printf("factorial %d=%d\n",n,m);
+    
+
+    return 0;
+}
