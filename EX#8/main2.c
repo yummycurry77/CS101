31b7c8efce1e25516451c8116d10b2957cd4c8a8
yummycurry77 <k0935225710@gmail.com>
2025-11-13 16:20:07 +0800

    Update main2.c

--- EX#8/main2.c
+++ EX#8/main2.c
@@ -1,44 +1,39 @@
-/******************************************************************************
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+#include<string.h>
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
-
-#define COUNTER_FILE "counter.bin"
+#define COUNETER_FILE "counter.bin"
 #define MAX_LOTTO_NUM 7
 #define MAX_LOTTO_NUMSET 5
 
 void init_file(){
     int write_array[1] = {0};
-    FILE* fp = fopen(COUNTER_FILE, "r");
-    if(fp == NULL) {
-     FILE* tmpfp = fopen(COUNTER_FILE, "wb+");
-     fwrite(write_array, sizeof(int), 1, tmpfp);
-     fclose(tmpfp);
-    }else {
+    FILE* fp = fopen(COUNETER_FILE, "r");
+    if(fp == NULL){
+        FILE* tmpfp = fopen(COUNETER_FILE, "wb+");
+        fwrite(write_array, sizeof(int), 1, tmpfp);
+        fclose(tmpfp);
+    }
+    else{
         fclose(fp);
     }
 }
 
-int get_counter() {
+int get_counter(){
     int read_array[1];
-    FILE* tmpfp = fopen(COUNTER_FILE, "rb");
+    FILE* tmpfp = fopen(COUNETER_FILE, "rb");
     fread(read_array, sizeof(int), 1, tmpfp);
     fclose(tmpfp);
     return read_array[0];
-}    
+}
 
-int num_in_numset(int num, int numset[], int Len) {
+
+
+int num_in_numset(int num, int numset[], int Len){
     int ret = 0;
-    for(int i = 0; i< Len; i++){
-        if (num == numset[i]) {
+    for(int i=0; i<Len; i++){
+        if(num == numset[i]){
             ret = 1;
             break;
         }
@@ -46,46 +41,59 @@ int num_in_numset(int num, int numset[], int Len) {
     return ret;
 }
 
-void print_lotto_row(FILE* tmpfp, int n) {
-    int numset[MAX_LOTTO_NUM];
+
+
+void set_counter(int counter){
+    int write_array[1];
+    write_array[0] = counter;
+    FILE* tmpfp = fopen(COUNETER_FILE, "wb");
+    fwrite(write_array, sizeof(int), 1, tmpfp);
+    fclose(tmpfp);
+}
+
+void print_lotto_row(FILE* tmpfp, int n){
+    int numset[MAX_LOTTO_NUMSET];
     
     fprintf(tmpfp, "[%d]: ", n);
-    for(int i = 0; i<MAX_LOTTO_NUM-1; ) {
-        int num = (rand() % 69) + 1;
-        if(num_in_numset(num, numset, MAX_LOTTO_NUM-1)) {
+    for(int i=0; i<MAX_LOTTO_NUM-1;){
+        int num = (rand()%69) + 1;
+        if(num_in_numset(num, numset, MAX_LOTTO_NUM-1)){
             continue;
-        } else {
+        }
+        else{
             numset[i] = num;
             i++;
-        } 
+        }
     }
-    
-    for(int i = 0; i < 1; ) {
-        int num = (rand() % 10) + 1;
-        if(num_in_numset(num, numset, MAX_LOTTO_NUM-1)) {
+    for(int i=0; i<1; ){
+        int num = (rand()%10) + 1;
+        if(num_in_numset(num, numset, MAX_LOTTO_NUM-1)){
             continue;
-        } else {
+        }
+        else{
             numset[MAX_LOTTO_NUM-1] = num;
             i++;
-        } 
+        }
     }
     
-    for(int i = 0; i < MAX_LOTTO_NUM-1; ++i) {
-        for(int j = 0; j < i; ++j) {
-            if (numset[j] > numset[i]) {
+    for(int i=0; i<=MAX_LOTTO_NUM-1;i++){
+        for(int j = 0; j<=i; j++){
+            if(numset[j]>numset[i]){
                 int temp = numset[j];
                 numset[j] = numset[i];
                 numset[i] = temp;
             }
         }
+        
     }
     
-    for(int i =0; i< MAX_LOTTO_NUM; i++) {
-        fprintf(tmpfp, "%02d ", numset[i]);
+    for(int i=0; i<MAX_LOTTO_NUM; i++){
+       fprintf(tmpfp, "%02d ", numset[i]);
     }
     fprintf(tmpfp, "\n");
 }
-void print_lottofile(int num_set, int counter, char lotto_file[]) {
+
+void print_lottofile(int num_set, int counter, char lotto_file[]){
     time_t curtime;
     time(&curtime);
     srand(time(0));
@@ -93,12 +101,13 @@ void print_lottofile(int num_set, int counter, char lotto_file[]) {
     FILE* tmpfp = fopen(lotto_file, "w+");
     fprintf(tmpfp, "========= lotto649 =========\n");
     fprintf(tmpfp, "========+ No.%05d +========\n", counter);
-    fprintf(tmpfp, "= %. *s =\n", 24, ctime(&curtime));
+    fprintf(tmpfp, "= %.*s=\n", 24, ctime(&curtime));
     
-    for(int i = 0; i <MAX_LOTTO_NUMSET;i++){
-        if(i < num_set) {
+    for(int i=0; i<MAX_LOTTO_NUMSET; i++){
+        if(i < num_set){
             print_lotto_row(tmpfp, i+1);
-        } else {
+        }
+        else{
             fprintf(tmpfp, "[%d]: -- -- -- -- -- -- --\n", i+1);
         }
     }
@@ -106,28 +115,23 @@ void print_lottofile(int num_set, int counter, char lotto_file[]) {
     fprintf(tmpfp, "========= csie@CGU =========\n");
     fclose(tmpfp);
 }
-void do_lotto_main(int counter) {
+
+void do_lotto_main(int counter){
     char lotto_file[32];
     int num_set = 0;
     snprintf(lotto_file, 32, "lotto[%05d].txt", counter);
-    printf("歡迎光臨長庚樂透彩購買機台\n");
-    printf("請問您要買幾組樂透彩 : ");
+    printf("歡迎光臨長庚樂透採購機台\n");
+    printf("請問你要買幾組樂透彩：\n");
     scanf("%d", &num_set);
     print_lottofile(num_set, counter, lotto_file);
-    printf("已為您購買的 %d 組樂透組合輸出至 %S\n", num_set, lotto_file);
+    printf("已為您購買的 %d 組樂透組合輸出至 %s\n", num_set, lotto_file);
 }
-void set_counter(int counter) {
-    int write_array[1];
-    write_array[0] = counter;
-    FILE* tmpfp = fopen(COUNTER_FILE, "wb");
-    fwrite(write_array, sizeof(int), 1, tmpfp);
-    fclose(tmpfp);
-}
-int main()
-{
+
+int main(){
     int counter;
     init_file();
-    counter = get_counter();printf("counter = %d\n", counter);
+    counter = get_counter();
+    printf("counter = %d\n", counter);
     do_lotto_main(++counter);
     set_counter(counter);
     return 0;
